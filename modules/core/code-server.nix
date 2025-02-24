e65c987c707ca3cdc981e515ef14358dc88ebbcd
João Vithor Maia Alves <163915303+MrKatso@users.noreply.github.com>
2025-02-23 22:10:20 -0300

    Some important changes

---
+++ modules/core/code-server.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  # -------------------------- #
+  # CODE-SERVER SETUP SETTINGS #
+  # -------------------------- #
+  services.code-server = {
+    enable = true;
+    user = "mr";
+    group = "users";
+    auth = "none";
+    port = 1836;
+    disableTelemetry = true;
+    disableWorkspaceTrust = true;
+    disableUpdateCheck = true;
+    disableFileDownloads = true;
+    disableGettingStartedOverride = true;
+  };
+
+  # --------------------------- #
+  # CODE-SERVER PACKAGE INSTALL #
+  # --------------------------- #
+  environment.systemPackages = with pkgs; [
+    pkgs.code-server
+  ];
+}
