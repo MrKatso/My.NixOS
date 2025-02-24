fe1fcdbb683ed1d62dea0d5d87752ddf23cf6747
João Vithor Maia Alves <163915303+MrKatso@users.noreply.github.com>
2025-02-23 22:34:25 -0300

    Some important changes

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -1,8 +1,26 @@
 { config, pkgs, lib, ... }:
 
 {
+  # ------------------------- #
+  # MASTER VMs SYSTEM IMPORTS #
+  # ------------------------- #
   imports = [
     ./hardware-configuration.nix
     ./../../modules/core
   ];
+
+  # ------------------ #
+  # SSH SETUP SETTINGS #
+  # ------------------ #
+  services.openssh = {
+    enable = true;
+    openFirewall = false;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "yes";
+      # AllowUsers = [ "mr" ];
+      # AllowGroups = [ "networkmanager" "wheel" ];
+    };
+  };
 }
