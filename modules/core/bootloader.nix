05ce204724dab560a16f57ef429b98fce31fbd70
Mr.Katso <joaomaia.vt@gmail.com>
2025-02-25 19:25:48 -0300

    Update with important changes

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,9 +4,16 @@
   # ------------------------- #
   # BOOTLOADER SETUP SETTINGS #
   # ------------------------- #
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader = {
+    systemd-boot.enable = true;
+    systemd-boot.configurationLimit = 10;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # ------------------------ #
+  # BOOT FILESYSTEMS OPTIONS #
+  #    IN DUAL BOOT ONLY     #
+  # ------------------------ #
   boot.supportedFilesystems = [ "ntfs" ];
 
   # --------------------- #
