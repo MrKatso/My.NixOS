c380c37ef2441674e8d2ddfff07960ab2e01196e
mr <joaomaia.vt@gmail.com>
2025-02-26 22:02:42 -0300

    Boot Filesystems management

---
+++ modules/core/desktop/boot-fs.nix
@@ -0,0 +1,14 @@
+{
+  # ------------------------ #
+  # BOOT FILESYSTEMS OPTIONS #
+  #    IN DUAL BOOT ONLY     #
+  # ------------------------ #
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # ---------------------------- #
+  # NTFS NEEDED PACKAGES INSTALL #
+  # ---------------------------- #
+  environment.systemPackages = with pkgs; [
+    ntfs-3g
+  ];
+}
